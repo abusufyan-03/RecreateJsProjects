c641c6ee22d94cf7f68b5f14da4b54d8417a5e5b
Sufyan <sufiyan.manuu@gmail.com>
2025-09-07 23:56:14 +0530

    Project: Unlimited color

---
+++ 04-unlimitedColor/unlimitedColor.js
@@ -0,0 +1,28 @@
+function randomColor(){
+  const hex = '0123456789abcdef';
+  let color = '#';
+
+  for(let i = 0; i<6; i++){
+    color = color + hex[Math.floor(Math.random() * 16)];
+  }
+  return color
+}
+
+let valId
+console.log("after declaring valId: ", valId)
+document.querySelector('#start').addEventListener('click', function(){
+ if(!valId){
+   valId = setInterval(function(){
+    document.body.style.backgroundColor = randomColor();
+  }, 1000)
+ }
+ console.log('after clicking start button valId', valId )
+});
+
+document.querySelector('#stop').addEventListener('click', function(){
+  clearInterval(valId);
+   console.log('after clicking stop button valId', valId )
+  valId = null
+   console.log('after clicking stop 2 button valId', valId )
+
+})
